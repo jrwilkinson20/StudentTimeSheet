024223fee3ac84e8f96f89d9b9eafb597142e662
tjcj211 <timcarta@hotmail.com>
2021-04-06 15:48:04 -0400

    WIP for routing

--- Models/account.js
+++ Models/account.js
@@ -1,36 +1,85 @@
 //Timothy Carta, Victoria Gorski, Julia Wilkinson
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const classes = require("../Models/class");
 
- var accountSchema = new Schema({
+var accountSchema = new Schema({
 	username: {
 		type: String,
 		required: Boolean,
-		unique: Boolean
+		unique: Boolean,
 	},
 	email: {
 		type: String,
 		required: Boolean,
-		unique: Boolean
+		unique: Boolean,
 	},
 	password: {
 		type: String,
 		required: Boolean,
-		unique: Boolean
+		unique: Boolean,
 	},
 	account_type: {
 		type: String,
 		required: Boolean,
 		unique: Boolean,
-		enum: ['Professor', 'Student'], 
-		default: 'Student'
+		enum: ['Professor', 'Student'],
+		default: 'Student',
 	},
-	class: {
-		type: [{type: Schema.ObjectId, ref: 'classes'}],
-		required: Boolean,
-		unique: Boolean
-	}
+	class: [classSchema],
 });
-var accountSchema = mongoose.model("accounts", accountSchema);
+
+var classSchema = new Schema({
+	name: {
+		type: String,
+		required: Boolean,
+		unique: Boolean,
+	},
+	lesson: {
+		type: [lessonSchema],
+		required: Boolean,
+		unique: Boolean,
+	},
+	class_code: {
+		type: String,
+		required: Boolean,
+		unique: Boolean,
+	},
+});
+
+var lessonSchema = new Schema({
+	name: {
+		type: String,
+		required: Boolean,
+		unique: Boolean,
+	},
+
+	record: {
+		type: [recordSchema],
+		required: Boolean,
+		unique: Boolean,
+	},
+
+	due_date: {
+		type: Date,
+		required: Boolean,
+		unique: Boolean,
+	},
+});
+
+var recordSchema = new Schema({
+	type: {
+		type: String,
+		required: Boolean,
+		unique: Boolean,
+		enum: ['Homework', 'Exam Prep', 'Studying'],
+		default: 'Homework',
+	},
+	minutes: {
+		type: Number,
+		required: Boolean,
+		unique: Boolean,
+	},
+});
+
+var accountSchema = mongoose.model('accounts', accountSchema);
 module.exports = accountSchema;
--- routes/interactions.js
+++ routes/interactions.js
@@ -13,6 +13,7 @@ interactionsRouter
 	.all((req, res, next) => {
 		next();
 	})
+	//Get all accounts
 	.get((req, res, next) => {
 		accounts.find({}, (err, account) => {
 			if (err) throw err;
@@ -20,55 +21,68 @@ interactionsRouter
 		});
 		res.end();
 	})
+	//Insert account
 	.post((req, res, next) => {
 		accounts.create(req.body, (err, account) => {
 			if (err) throw err;
-			console.log('Account Created');
+			console.log('Account Created with id: ' + account._id);
 		});
 		res.end();
 	});
 
+//AccountId
 // /accounts/ACCOUNTID
-interactionsRouter.route('/accountId').put((req, res, next) => {
-	accounts.findByIdAndUpdate(
-		req.params.accountId,
-		{ $set: req.body },
-		{ new: true },
-		(err, account) => {
-			if (err) throw err;
-			res.json(account);
-		}
-	);
-});
+interactionsRouter
+	.route('/:accountId')
+	// update account
+	.put((req, res, next) => {
+		accounts.findByIdAndUpdate(
+			req.params.accountId,
+			{ $set: req.body },
+			{ new: true },
+			(err, account) => {
+				if (err) throw err;
+				res.json(account);
+			}
+		);
+		res.end();
+	});
 
 // /accounts/ACCOUNTID/classes/
 // Classes for a given account id
-interactionsRouter.route('/:accountId/classes/').get((req, res, next) => {
-	accounts.findById(req.params.accountId, (err, account) => {
-		if (err) throw err;
-		res.json(account.class);
-	});
-	res.end();
-});
-
-// /accounts/ACCOUNTID/classes/ClassID/
 interactionsRouter
-	.route('/:accountId/classes/:classId/')
+	.route('/:accountId/classes/')
+	//Get all classes for given account id
 	.get((req, res, next) => {
 		accounts.findById(req.params.accountId, (err, account) => {
 			if (err) throw err;
-			account.findById(req.params.classId, (err, clas) => {
-				if (err) throw err;
-				res.json(account.clas.id(req.params.classId));
-			});
+			res.json(account.class);
+		});
+		res.end();
+	});
+
+//Class Ids
+// /accounts/ACCOUNTID/classes/ClassID/
+interactionsRouter
+	.route('/:accountId/classes/:classId/')
+	//get specific classId
+	.get((req, res, next) => {
+		accounts.findById(req.params.accountId, (err, account) => {
+			if (err) throw err;
+			res.json(account.class.id(req.params.classId));
 		});
 		res.end();
 	})
+	//Create a new class with id ClassId
 	.post((req, res, next) => {
 		classes.create(req.body, (err, classes) => {
 			if (err) throw err;
-
-			console.log('Account Created');
+			accounts.class.push(req.body);
+			accounts.save((err, account) => {
+				if (err) throw err;
+				res.json(account);
+			});
+			console.log('Class Created');
 		});
 		res.end();
 	});
